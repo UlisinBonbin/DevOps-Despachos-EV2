57787861e54486dc9918589627c83aeae2e2501a
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-05-15 18:25:56 -0400

    Cambios para el aws

--- src/main/java/com/citt/config/OpenApiConfig.java
+++ src/main/java/com/citt/config/OpenApiConfig.java
@@ -3,7 +3,6 @@ package com.citt.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
